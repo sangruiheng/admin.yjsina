65b538097257a471a7a16ecaed77a575aeb5ad17
桑瑞恒 <33930319+sangruiheng@users.noreply.github.com>
2019-07-27 15:17:41 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,13 +7,13 @@ return array(
     'ERROR_PAGE' => '/error.html',//错误和异常页面链接
     'LOAD_EXT_CONFIG' => 'const',  //自定义全局常量
     //数据库链接
-    'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'yanjiao', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => 'root', // 密码
+    'DB_TYPE' => '', // 数据库类型
+    'DB_HOST' => '', // 服务器地址
+    'DB_NAME' => '', // 数据库名
+    'DB_USER' => '', // 用户名
+    'DB_PWD' => '', // 密码
     'DB_PORT' => 3306, // 端口
-    'DB_PREFIX' => 'icpnt_', // 数据库表前缀
+    'DB_PREFIX' => '', // 数据库表前缀
     'DB_CHARSET' => 'utf8', // 字符集
     'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
     'DB_PARAMS' => array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL),//数据库字段名大小写敏感
